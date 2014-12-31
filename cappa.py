d9a9d8dcc4b3bb1b3be7b7afeda9351fc5cede84
Yoriyasu Yano <yorinasub17@gmail.com>
2014-12-30 19:20:11 -0800

    Support system level packages

--- cappa.py
+++ cappa.py
@@ -1,6 +1,7 @@
 import cytoolz
 import operator
 import subprocess
+import platform
 from distutils.spawn import find_executable
 
 class MissingExecutable(Exception):
@@ -30,8 +31,14 @@ class CapPA(object):
                 raise MissingExecutable('pip not found')
 
     def _install_package_dict(self, package_dict):
+        if 'sys' in package_dict:
+            self._install_system_packages(package_dict['sys'])
+
         for key, packages in package_dict.iteritems():
-            if key == 'npmg':
+            if key == 'sys':
+                # system packages are handled separately above
+                continue
+            elif key == 'npmg':
                 options = ['-g']
                 key = 'npm'
             else:
@@ -59,15 +66,43 @@ class CapPA(object):
     def _install_package_list(self, packages):
         split = map(CapPA.extract_manager, packages)
         for key, packages in cytoolz.itertoolz.groupby(operator.itemgetter(0), split).iteritems():
-            manager = getattr(self, key)
-            self._assert_manager_exists(key, manager)
-            options = list(set(sum(map(operator.itemgetter(2), packages), [])))
-            packages = map(operator.itemgetter(1), packages)
-            subprocess.check_call([manager] + options + ['install'] + packages)
+            if key == 'sys':
+                # System packages are handled separately
+                packages = map(operator.itemgetter(1), packages)
+                self._install_system_packages(packages)
+            else:
+                manager = getattr(self, key)
+                self._assert_manager_exists(key, manager)
+                options = list(set(sum(map(operator.itemgetter(2), packages), [])))
+                packages = map(operator.itemgetter(1), packages)
+                subprocess.check_call([manager] + options + ['install'] + packages)
+
+    def _install_system_packages(self, packages):
+        # For now, only ubuntu is supported
+        if platform.dist()[0] != 'Ubuntu':
+            raise UnknownManager('System packages only supported on Ubuntu')
+
+        if isinstance(packages, dict):
+            # Convert to list, since apt doesn't support installing specific
+            # versions
+            packages = packages.keys()
+
+        import apt
+        # Update cache
+        cache = apt.cache.Cache()
+        cache.update()
+
+        # Now install all the packages requested
+        for package in packages:
+            pkg = cache[package]
+            if not pkg.is_installed:
+                pkg.mark_install()
+        cache.commit()
 
     @staticmethod
     def extract_manager(package):
-        if not (package.startswith('pip') or package.startswith('bower') or package.startswith('npm')):
+        if not (package.startswith('pip') or package.startswith('bower') or
+                package.startswith('npm')):
             raise UnknownManager('Could not identify base package manager for \'{}\''.format(package))
 
         split = package.split('-')
