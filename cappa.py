8781f42f20bcd6a7acffd61caeee60ec7ff3cc2f
Yoriyasu Yano <yorinasub17@gmail.com>
2015-01-24 22:08:27 -0800

    Use sudo for apt related sections

--- cappa.py
+++ cappa.py
@@ -21,6 +21,8 @@ class CapPA(object):
         self.npm = find_executable('npm')
         self.bower = find_executable('bower')
         self.pip = find_executable('pip')
+        # TODO: support for osx
+        self.sys = find_executable('apt-get')
         self.warn_mode = warn_mode
         self.private_https_oauth = private_https_oauth
 
@@ -38,24 +40,27 @@ class CapPA(object):
                 raise MissingExecutable('bower not found')
             if manager_type == 'pip':
                 raise MissingExecutable('pip not found')
+            if manager_type == 'sys':
+                raise MissingExecutable('apt-get not found')
 
     def _install_package_dict(self, package_dict):
         if 'sys' in package_dict:
-            self._install_system_packages(package_dict['sys'])
+            self._update_apt_cache()
 
         for key, packages in package_dict.iteritems():
             try:
-                if key == 'sys':
-                    # system packages are handled separately above
-                    continue
-                elif key == 'Captricity':
+                if key == 'Captricity':
                     self._private_package_dict(packages)
                     self._install_package_dict(packages)
                     continue
                 elif key == 'npmg':
+                    prefix = []
                     options = ['-g']
                     key = 'npm'
+                elif key == 'sys':
+                    prefix = ['sudo']
                 else:
+                    prefix = []
                     options = []
 
                 if key == 'npm':
@@ -64,14 +69,16 @@ class CapPA(object):
                     connector = '#'
                 elif key == 'pip':
                     connector = '=='
+                elif key == 'sys':
+                    connector = None # does not support versioning
                 else:
                     raise UnknownManager('Could not identify base package manager \'{}\''.format(key))
 
                 manager = getattr(self, key)
                 self._assert_manager_exists(key, manager)
-                args = [manager, 'install'] + options
+                args = prefix + [manager, 'install'] + options
                 for package, version in packages.iteritems():
-                    if version is None:
+                    if version is None or connector is None:
                         args.append(package)
                     else:
                         args.append(package + connector + version)
@@ -85,18 +92,17 @@ class CapPA(object):
     def _install_package_list(self, packages):
         split = map(CapPA.extract_manager, packages)
         for key, packages in cytoolz.itertoolz.groupby(operator.itemgetter(0), split).iteritems():
+            manager = getattr(self, key)
+            self._assert_manager_exists(key, manager)
+            options = list(set(sum(map(operator.itemgetter(2), packages), [])))
+            packages = map(operator.itemgetter(1), packages)
             if key == 'sys':
-                # System packages are handled separately
-                packages = map(operator.itemgetter(1), packages)
-                self._install_system_packages(packages)
+                prefix = ['sudo']
             else:
-                manager = getattr(self, key)
-                self._assert_manager_exists(key, manager)
-                options = list(set(sum(map(operator.itemgetter(2), packages), [])))
-                packages = map(operator.itemgetter(1), packages)
-                subprocess.check_call([manager] + options + ['install'] + packages)
+                prefix = []
+            subprocess.check_call(prefix + [manager] + options + ['install'] + packages)
 
-    def _install_system_packages(self, packages):
+    def _update_apt_cache(self):
         # For now, only ubuntu is supported
         if platform.dist()[0] != 'Ubuntu':
             message = 'System packages only supported on Ubuntu'
@@ -106,23 +112,7 @@ class CapPA(object):
             else:
                 raise UnknownManager(message)
 
-        if isinstance(packages, dict):
-            # Convert to list, since apt doesn't support installing specific
-            # versions
-            packages = packages.keys()
-
-        import apt
-        # Update cache
-        cache = apt.cache.Cache()
-        cache.update()
-        cache.open()
-
-        # Now install all the packages requested
-        for package in packages:
-            pkg = cache[package]
-            if not pkg.is_installed:
-                pkg.mark_install()
-        cache.commit()
+        subprocess.check_call(['sudo', 'apt-get', 'update'])
 
     def _private_package_dict(self, package_dict):
         # reconstruct package_dict based on private repo handling mode
