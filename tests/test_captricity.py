0aa5a5bebe347645a8a7acf55cbb622603a62e5f
Wei You <weiy@captricity.com>
2015-07-13 09:55:42 -0700

    captricity 80% done

--- tests/test_captricity.py
+++ tests/test_captricity.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 import os
 from fabric.api import env, execute, task, sudo, put, run, cd
+from cStringIO import StringIO
+
 
 from .base import VagrantTestCase
 
@@ -9,18 +11,30 @@ class CaptricityTestCases(VagrantTestCase):
     def test_basic(self):
         self.install_requirements_json(TEST_NECESSARY_PROVISIONERS)
         token = raw_input("Please enter your Github personal access tokens: ")
-        self.run_fabric_task(self.setup_github_token_factory(token))
-        self.install_requirements_json(TEST_INSTALL_CAPTRICITY_VERSION_JSON)
-        # self.run_spec('captricity_install_basic_spec')
-        print('Success!!!!!!!!!!!!!!!!!!!!!!!!')
+        # self.run_fabric_task(self.setup_github_token_factory(token))
+        self.run_fabric_task(self.install_requirements_json_captricity_factory(TEST_INSTALL_CAPTRICITY_VERSION_JSON, token))
+        self.run_spec('captricity_install_basic_spec')
+        # print('Success!!!!!!!!!!!!!!!!!!!!!!!!')
 
-    def setup_github_token_factory(self, token):
+    # def setup_github_token_factory(self, token):
+    #
+    #     @task
+    #     def setup_github_token():
+    #         with cd('/home/vagrant'):
+    #             os.environ['GITHUB_TOKEN'] = token
+    #
+    #     return setup_github_token
+
+    def install_requirements_json_captricity_factory(self, requirements_json, token):
+
         @task
-        def setup_github_token():
+        def install_requirements_json_captricity():
             with cd('/home/vagrant'):
-                os.environ['GITHUB_TOKEN'] = token
-                
-        return setup_github_token
+                put(StringIO(requirements_json), '/home/vagrant/requirements.json')
+                os.putenv('GITHUB_TOKEN', token)
+                run('cappa install --private-https-oauth --no-venv -r /home/vagrant/requirements.json')
+        return install_requirements_json_captricity
+
 
 TEST_INSTALL_CAPTRICITY_VERSION_JSON = """{
     "Captricity": {
