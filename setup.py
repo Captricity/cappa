c2d5c9807f5d3da7061bc4f8afaf6e287faa450b
Hui Peng Hu <woohp135@gmail.com>
2016-02-25 11:35:38 -0800

    bower installs using force

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name = "cappa",
-    version = "0.10",
-    description = "Package installer for Captricity. Supports apt-get, pip, bower, and npm.",
-    author = "Yoriyasu Yano",
-    author_email = "yorinasub17@gmail.com",
+    name="cappa",
+    version="0.11",
+    description="Package installer for Captricity. Supports apt-get, pip, bower, and npm.",
+    author="Yoriyasu Yano",
+    author_email="yorinasub17@gmail.com",
 
-    py_modules = ["cappa"],
-    scripts = ["scripts/cappa"],
-    install_requires = open("requirements.txt").read().split(),
-    tests_requires = open("test_requirements.txt").read().split(),
+    py_modules=["cappa"],
+    scripts=["scripts/cappa"],
+    install_requires=open("requirements.txt").read().split(),
+    tests_requires=open("test_requirements.txt").read().split(),
     test_suite='tests'
 )
