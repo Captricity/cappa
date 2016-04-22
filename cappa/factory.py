a66d15d95e7ec62da12ccade0894e78e8dba6673
Yoriyasu Yano <yorinasub17@gmail.com>
2016-04-22 01:51:46 -0700

    Switch if-else block to dictionary lookup

--- cappa/factory.py
+++ cappa/factory.py
@@ -12,28 +12,29 @@ from .tsd import Tsd
 from .private.pip import PrivatePip
 
 
+MANAGER_MAP = {
+    'pip': Pip,
+    'pip3': Pip3,
+    'pip_pypy': PipPypy,
+    'sys': Apt,
+    'npm': Npm,
+    'npmg': NpmG,
+    'bower': Bower,
+    'tsd': Tsd
+}
+PRIVATE_MANAGER_MAP = {
+    'pip': PrivatePip
+}
+
+
 def manager_key_to_cappa(manager_key):
-    if manager_key == 'pip':
-        return Pip
-    elif manager_key == 'pip3':
-        return Pip3
-    elif manager_key == 'pip_pypy':
-        return PipPypy
-    elif manager_key == 'sys':
-        return Apt
-    elif manager_key == 'npm':
-        return Npm
-    elif manager_key == 'npmg':
-        return NpmG
-    elif manager_key == 'bower':
-        return Bower
-    elif manager_key == 'tsd':
-        return Tsd
+    if manager_key in MANAGER_MAP:
+        return MANAGER_MAP[manager_key]
     else:
         raise UnknownManager('{} is not a supported manager.'.format(manager_key))
 
 def private_manager_key_to_cappa(manager_key):
-    if manager_key == 'pip':
+    if manager_key in PRIVATE_MANAGER_MAP:
         return PrivatePip
     else:
         raise UnknownManager('{} is not a supported private repo manager.'.format(manager_key))
