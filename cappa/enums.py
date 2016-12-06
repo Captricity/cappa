c222e5b892ae6f6873815fc5e3d8625dd739db6c
AJ Renold <ajr@AJs-MacBook-Pro.local>
2016-12-06 12:50:48 -0800

    is ubuntu

--- cappa/enums.py
+++ cappa/enums.py
@@ -2,5 +2,5 @@ import platform
 
 
 IS_MAC = 'Darwin' in platform.platform(terse=1)
-IS_UBUNTU = platform.dist()[0] == 'Ubuntu'
+IS_UBUNTU = platform.dist()[0] == 'Ubuntu' or platform.dist()[0] == 'debian'
 ALL_MANAGERS = ('npm', 'npmg', 'yarn', 'yarng', 'bower', 'tsd', 'pip', 'pip3', 'pip_pypy', 'sys', 'Captricity')
