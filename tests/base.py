c0881024290f5d7852b1466fe26223f3f00d7a8d
Jose Pagan <josep@vidado.ai>
2022-11-02 12:29:11 -0700

    removing mkdir because /cappa/test dir exists

--- tests/base.py
+++ tests/base.py
@@ -22,7 +22,6 @@ def install_requirements_without_virtualenv(requirements_json):
 
     @task
     def requirements_json_install():
-        os.mkdir('/cappa/test')
         os.chdir('/cappa/test')
         put({StringIO(requirements_json)}, '/cappa/test/requirements.json')
         call('cappa install --no-venv -r /cappa/test/requirements.json', shell=True) 
